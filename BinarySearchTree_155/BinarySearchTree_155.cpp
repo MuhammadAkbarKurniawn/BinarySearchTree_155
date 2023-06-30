513c0a1c10ab4a20b5ee63ddfc23d2c63a857e24
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2023-06-30 18:14:08 +0700

    Membuat logaritma insert pada inputan binary search

--- BinarySearchTree_155/BinarySearchTree_155.cpp
+++ BinarySearchTree_155/BinarySearchTree_155.cpp
@@ -27,4 +27,16 @@ public:
 	{
 		ROOT = NULL; // initializing ROOT  to NULL
 	}
+
+	void insert(string element) // insert a node in the binary search tree
+	{
+		Node* newNode = new Node(element, NULL, NULL);	// Allocate memory for the new Node
+		newNode->info = element;	// Assign value to the data field of the new node
+		newNode->leftchild = NULL;	// Make the left child of the new node point to NULL
+		newNode->rightchild = NULL;	// Make the right child of the new node point to NULL
+
+		Node* parent = NULL;	
+		Node* currentNode = NULL;
+		search(element, parent, currentNode);	// locate the node which will be the parent of the node to be interested
+	}
 };

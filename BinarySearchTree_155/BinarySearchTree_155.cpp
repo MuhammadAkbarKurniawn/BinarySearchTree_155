2300fe3cc8e017bb072f350e2da3cdc4918e1dd4
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2023-07-02 17:46:52 +0700

    BinarySearchTree fix

--- BinarySearchTree_155/BinarySearchTree_155.cpp
+++ BinarySearchTree_155/BinarySearchTree_155.cpp
@@ -123,7 +123,7 @@ int main()
 	BinaryTree obj;
 	while (true)
 	{
-		cout << "\Menu" << endl;
+		cout << "\nMenu" << endl;
 		cout << "1. Implement insert operation" << endl;
 		cout << "2. Perform inorder traversal" << endl;
 		cout << "3. Perform preorder traversal" << endl;
